e9d313a0fbac1621ac2da03d46426bd9c5a5665b
Guzman Monne <guzmonne@hotmail.com>
2018-10-28 23:59:59 -0300

    updated the participant create request to update the labs participants list

--- controllers/participants.js
+++ controllers/participants.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const union = require('lodash/union.js');
 
-const model = require('../models/participants.js');
+const utils = require('../modules/utils.js');
+const participants = require('../models/participants.js');
+const labs = require('../models/labs.js');
 const crudController = require('./crudController.js');
 
 /** Create the express router */
@@ -9,8 +12,30 @@ const router = express.Router();
 /** Exports */
 exports = module.exports = router;
 
+/** Routes **/
+router.post('/', (req, res) => {
+  const body = req.body;
+
+  if (body === undefined) return utils.handleError(res, '"body" is undefined');
+
+  const labId = body.labId;
+
+  if (labId === undefined)
+    return utils.handleError(res, '"labId" is undefined');
+
+  const participant = participants.create(body);
+
+  const lab = labs.get(labId);
+
+  labs.update(lab.id, {
+    participants: union(lab.participants, [participant.id])
+  });
+
+  return utils.handleSuccess(res, participant);
+});
+
 /** CRUD Routes */
-crudController(router, model, {
+crudController(router, participants, {
   type: 'participants',
   type: 10,
   maxLimit: 100,
